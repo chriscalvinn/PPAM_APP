bd57aff4b088ba9ab8fe6f6d59d7cc46bdfc75a3
Your Name <you@example.com>
2020-05-12 00:38:00 +0700

    tambah layar

--- app/src/main/java/com/example/ppam/NextLevel.java
+++ app/src/main/java/com/example/ppam/NextLevel.java
@@ -1,6 +1,9 @@
 package com.example.ppam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import android.os.Bundle;
 
@@ -10,5 +13,11 @@ public class NextLevel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_level);
+    }
+
+    public void onButtonClick(View v){
+        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+        //myIntent.putExtra("nextLevel", "Barcelona");
+        startActivity(myIntent);
     }
 }
