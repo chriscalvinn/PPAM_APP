bf8f8f0a749cfa12a05c6a94e999d30402d203e7
Your Name <you@example.com>
2020-05-12 01:26:16 +0700

    bikin method buat pindah ke main

--- app/src/main/java/com/example/ppam/NextLevel.java
+++ app/src/main/java/com/example/ppam/NextLevel.java
@@ -5,9 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 public class NextLevel extends AppCompatActivity {
+    final String arr[] = {"mainan", "anak muda", "bangunan"};
+    int level = 0;
+    String response;// = {"success":"true","message":"benar"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,16 @@ public class NextLevel extends AppCompatActivity {
         setContentView(R.layout.activity_next_level);
     }
 
-    public void onButtonClick(View v){
+    public void nextLevel(View v){
+        JSONObject reader = new JSONObject(response);
+        boolean bool = reader.getBoolean("success");
+        message = reader.getString("message");
+
         Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
-        //myIntent.putExtra("nextLevel", "Barcelona");
+        if (bool){
+        level++;
+        }
+        myIntent.putExtra("nextLevel", arr[level]);
         startActivity(myIntent);
     }
 }
